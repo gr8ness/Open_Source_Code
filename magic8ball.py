f5012b5b52792ff12efe1bd7e15df163e211886a
Deshone Henry <deshonehenry@gmail.com>
2022-04-02 11:16:22 -0400

    update code

--- magic8ball.py
+++ magic8ball.py
@@ -1,8 +1,21 @@
 import random
+import time
 
+#magic 8 ball
 print("Welcome to the Magic 8 Ball!")
 
-input("Ask a question:")
+#input a question
+input("Ask a question: ")
+
+#random list of words
 words = ("Yes","No","Maybe","Try Again","You're Funny! LOL")
 
+
+print("Thinking...")
+
+#delay answer for 3 secs
+time.sleep(2.0)
+
+
+#prints random words
 print(random.choice(words))
